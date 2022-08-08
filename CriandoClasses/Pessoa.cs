435ac628322f094c02a45f47468eb0c1b4a7eae6
2jdmtec <2jdmtec@LAB2-16>
2022-08-08 09:16:12 -0300

    Adicionar arquivos de projeto.

---
+++ CriandoClasses/Pessoa.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriandoClasses
+{
+    internal class Pessoa
+    {
+        private string nome;
+        private int idade;
+        private double altura;
+        private string sexo;
+
+        public Pessoa()
+        {
+            this.nome = null;
+            this.idade = 0;
+            this.altura = 0.0;
+            Sexo = null;
+        }
+        public  Pessoa(string nome, int idade, double altura, string sexo)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.altura = altura;
+            Sexo = sexo.ToString();
+
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public int Idade
+        {
+            get { return idade; }
+            set { idade = value; }
+        }
+
+        public double Altura
+        {
+            get { return altura; }
+            set { altura = value; }
+
+        }
+     public string Sexo
+        {
+            get { return sexo; }
+            set { sexo = value.ToUpper() == "F" ? "FEMININO" : "MASCULINO"; }
+        }
+
+        public double calcularPesoIdeal()
+        {
+            if (sexo.Equals("FEMININO"))
+                return (62.1 * altura) - 44.7;
+            else 
+                return (72.7 *altura) -58;
+
+        }
+
+        public override string ToString()
+        {
+            return (String.Format("Nome: {0} \n Idade: {1} \n Altura: {2} \n Sexo: {3} \n", nome, Idade, altura, sexo)); 
+
+
+        }
+
+    }
+}
---
+++ CriandoClasses/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriandoClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o nome da pessoa:"); string n = Console.ReadLine();
+            Console.Write("Digite a idade de {0}:",n );int i=int.Parse(Console.ReadLine());
+            Console.Write("Digite a altura de {0}:", n); double a = double.Parse(Console.ReadLine());
+            Console.Write("Digite a letra correspondente ao sexo(F/M):");
+            string s =Console.ReadLine();
+            Pessoa p = new Pessoa(n,i,a,s);
+            Console.WriteLine(p.ToString());
+          
+            Console.WriteLine("\n\n*** Dados da Pessoa***\n");
+            Console.WriteLine("Peso Ideal:{0}", p.calcularPesoIdeal());
+            Console.ReadKey();
+        }
+    }
+}
